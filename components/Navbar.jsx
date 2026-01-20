b7b14eb0da02aa8d20ed538066c81882dbf70336
MichalWadowskiEVS <80638034+Michau94@users.noreply.github.com>
2026-01-20 14:47:49 +0100

    mobile google login fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -272,10 +272,19 @@ const Navbar = () => {
               </Link>
             )}
             {!session && (
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4">
-                <i className="fa-brands fa-google mr-2"></i>
-                <span>Login or Register</span>
-              </button>
+              <>
+                {providers &&
+                  Object.values(providers).map((provider, index) => (
+                    <button
+                      className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
+                      key={index}
+                      onClick={() => signIn(provider.id)}
+                    >
+                      <FaGoogle className=" text-white mr-2" />
+                      <span>Login or Register</span>
+                    </button>
+                  ))}
+              </>
             )}
           </div>
         </div>
